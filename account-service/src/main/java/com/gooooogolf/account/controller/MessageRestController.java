21ec343874637ffe43aadef66b1d6796f485005c
Sirimongkol Panwa <sirimongkol.pan@ascendcorp.com>
2018-06-25 21:44:12 +0700

    Setup and sync account service to configuration-server

--- account-service/src/main/java/com/gooooogolf/account/controller/MessageRestController.java
+++ account-service/src/main/java/com/gooooogolf/account/controller/MessageRestController.java
@@ -1,13 +1,15 @@
 package com.gooooogolf.account.controller;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@RefreshScope
 @RestController
 public class MessageRestController {
 
-    @Value("${message:Hello account service default}")
+    @Value("${message:Hello default}")
     private String message;
 
     @RequestMapping("/message")
