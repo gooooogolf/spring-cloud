21b550a835cb40fcd39e5de65132baab9708d966
Sirimongkol Panwa <sirimongkol.pan@ascendcorp.com>
2018-06-25 21:57:17 +0700

    [Payment-Service] Set RefreshScope annotations for refresh runtime

--- payment-service/src/main/java/com/gooooogolf/payment/controller/MessageRestController.java
+++ payment-service/src/main/java/com/gooooogolf/payment/controller/MessageRestController.java
@@ -1,9 +1,11 @@
 package com.gooooogolf.payment.controller;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@RefreshScope
 @RestController
 public class MessageRestController {
 
