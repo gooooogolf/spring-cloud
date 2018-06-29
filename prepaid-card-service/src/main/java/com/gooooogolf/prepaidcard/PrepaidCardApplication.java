378ae40b7dc78b30d48dae5fafe1ce46029a94fa
Sirimongkol Panwa <sirimongkol.pan@ascendcorp.com>
2018-06-29 17:28:33 +0700

    [ppc-serivce] Init project

---
+++ prepaid-card-service/src/main/java/com/gooooogolf/prepaidcard/PrepaidCardApplication.java
@@ -0,0 +1,12 @@
+package com.gooooogolf.prepaidcard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrepaidCardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrepaidCardApplication.class, args);
+	}
+}
